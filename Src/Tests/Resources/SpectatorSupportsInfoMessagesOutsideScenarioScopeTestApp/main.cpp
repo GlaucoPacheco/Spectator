010452725fa02630120c553537badf48d3822859
Glauco Pacheco <glaucopacheco@gmail.com>
2026-01-11 12:25:52 -0300

    Added SpectatorSupportsInfoMessagesOutsideScenarioScopeTestApp test app.

---
+++ Src/Tests/Resources/SpectatorSupportsInfoMessagesOutsideScenarioScopeTestApp/main.cpp
@@ -0,0 +1,30 @@
+#include "ScenarioRepository.h"
+#include <Spectator.h>
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QDataStream>
+#include <QByteArray>
+#include <QString>
+#include <cstdio>
+
+using namespace Spectator;
+
+int main(int argc, char ** argv)
+{
+    QCoreApplication app(argc, argv);
+    QByteArray buffer;
+    buffer.reserve(1024);
+    QDataStream dataStream(&buffer, QIODeviceBase::WriteOnly);
+    auto & scenarioRepository = ScenarioRepository::global();
+    dataStream << scenarioRepository.size();
+    for (auto i = 0; i < scenarioRepository.size(); ++i)
+    {
+        const auto & scenario = scenarioRepository[i];
+        dataStream << QString(scenario.sourceFile());
+        dataStream << scenario.sourceLine();
+        dataStream << QString(scenario.name());
+    }
+    QTextStream outputStream(stdout);
+    outputStream << buffer.toBase64();
+    return 0;
+}
