087c13a6063a62c963574a04b384b65f601d0814
Glauco Pacheco <glaucopacheco@gmail.com>
2026-04-16 10:10:02 -0300

    Updated code on main README.md file. Removed large request count scenario file. Improved scenario with generators.

--- Src/Tutorial/ScenarioWithGenerator.cpp
+++ Src/Tutorial/ScenarioWithGenerator.cpp
@@ -3,28 +3,47 @@
 
 #include <Spectator>
 #include <QString>
+#include <map>
 
 using namespace Qt::StringLiterals;
 
 SCENARIO("Scenario with generator")
 {
-    // The whole scenario will be repeated twice
+    GIVEN("given executed once")
+    {
+        WHEN("when executed once")
+        {
+            THEN("then executed once")
+            {
+            }
+        }
+    }
+
+    // From now on, everything on the scenario paths will be run 2 times.
     const auto scenarioData = GENERATE(AS(QString), u"Hello"_s, u"World!"_s);
 
     GIVEN("a given with generate range")
     {
-        // This given will be repeated three times with givenData={1,2,3}
+        // From now on, everything on the scenario paths will be run 2*3 times (givenData iterates on {1,2,3})
         const auto givenData = GENERATE_RANGE(AS(int), 1, 3);
         REQUIRE(givenData == 1 || givenData == 2 || givenData == 3);
 
         WHEN("when with generate range with step")
         {
-            // This when will be repeated four times with whenData={1,3,5,7}
+            // From now on, everything on the scenario paths will be run 2*3*4 times (whenData iterates on {1,3,5,7})
             const auto whenData = GENERATE_RANGE_WITH_STEP(AS(int64_t), 1, 7, 2);
             REQUIRE(whenData == 1 || whenData == 3 || whenData == 5 || whenData == 7);
+
+            THEN("then with generate on maps")
+            {
+                // From now on, everything on the scenario path will be run 2*3*4*3 times.
+                const auto map = GENERATE(AS(std::map<int, QByteArray>), {{1,"one"}},
+                                                                         {{1,"one"}, {2,"two"}},
+                                                                         {{1,"one"}, {2,"two"}, {3,"three"}});
+            }
         }
     }
     // Let's generate some data at the scenario end just to test if Spectator
-    // will correctly repeat the whole scenario even without entering any section.
+    // correctly repeats the whole scenario even without entering any section.
     const auto scenarioEndData = GENERATE(AS(QString), u"a"_s, u"b"_s);
 }
